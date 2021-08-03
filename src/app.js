f4e6a35bcb3170a191d20b41573ee9f795435ac9
Shariq Kalim <shariqkalim123@gmail.com>
2021-08-03 18:14:21 +0530

    second

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/conn");
 
 const internships = require("./models/internships");
@@ -8,6 +9,7 @@ const router = require("./routers/intern");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
